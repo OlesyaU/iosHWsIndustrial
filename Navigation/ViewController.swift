bbf8b73ff7d3cefe5dabb04c481de6118e6846cd
OlesyaU <1600174@mail.ru>
2022-03-14 10:18:44 +0300

    Initial Commit

---
+++ Navigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Олеся on 10.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
