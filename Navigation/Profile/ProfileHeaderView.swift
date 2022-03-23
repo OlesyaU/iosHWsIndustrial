8a134d0eae781d90e196cee4da5c392cb701cdfe
OlesyaU <1600174@mail.ru>
2022-03-23 15:46:27 +0300

    Add UIelements to Profile VC and set constraints

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -8,6 +8,95 @@
 import UIKit
 
 class ProfileHeaderView: UIView {
+   
+    private let avatarImage: UIImageView = {
+        let image = UIImageView()
+        image.bounds.size = CGSize(width: 100, height: 100)
+        image.image = UIImage(named: "Томас")
+       image.contentMode = .scaleAspectFill
+        image.clipsToBounds = true
+        image.layer.borderWidth = 3
+        image.layer.borderColor = CGColor(red: 255, green: 255, blue: 255, alpha: 1)
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.layer.cornerRadius = image.bounds.height/2
+        return image
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel ()
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.textColor = .black
+        label.text = "Кот Томас"
+        label.numberOfLines = 1
+        label.layer.borderColor = CGColor(red: 255, green: 255, blue: 255, alpha: 1)
+        label.translatesAutoresizingMaskIntoConstraints = false
+      
+        return label
+    }()
+    
+    private let statusLabel: UILabel = {
+        let label = UILabel ()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .gray
+        label.text = "Тут будет статус"
+        label.numberOfLines = 0
+//        label.layer.borderColor = CGColor(red: 255, green: 255, blue: 255, alpha: 1)
+        label.translatesAutoresizingMaskIntoConstraints = false
+      
+        return label
+    }()
+    
+    
+    private let showStatusButton: UIButton = {
+        let button = UIButton()
+      button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 4
+        button.setTitle("Show status", for: .normal)
+        button.tintColor = .white
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        button.layer.shadowOffset = CGSize(width: 4, height: 4)
+        button.layer.shadowColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
+        button.layer.shadowRadius = 4
+        button.layer.shadowOpacity = 0.7
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        return button
+    }()
 
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        addSubview(avatarImage)
+        addSubview(nameLabel)
+        addSubview(showStatusButton)
+        addSubview(statusLabel)
+        showButtonConstraints()
+    
+    }
+    @objc private func buttonPressed() {
+        
+        print(statusLabel.text!)
+    }
+   
+    private func showButtonConstraints() {
+        NSLayoutConstraint.activate([
+            showStatusButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            showStatusButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            showStatusButton.heightAnchor.constraint(equalToConstant: 40),
+            showStatusButton.topAnchor.constraint(equalTo: avatarImage.bottomAnchor, constant: 16),
+            
+            avatarImage.topAnchor.constraint(equalTo: self.topAnchor, constant: 16),
+            avatarImage.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            avatarImage.widthAnchor.constraint(equalToConstant:100),
+            avatarImage.heightAnchor.constraint(equalToConstant: 100),
+            
+            nameLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 27),
+            nameLabel.leadingAnchor.constraint(equalTo: avatarImage.trailingAnchor),
+            nameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            nameLabel.heightAnchor.constraint(equalToConstant: 40),
+            
+            statusLabel.bottomAnchor.constraint(equalTo: showStatusButton.topAnchor, constant: -34),
+            statusLabel.leadingAnchor.constraint(equalTo: avatarImage.trailingAnchor)
+            ])
+    }
     
 }
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    private let profileHeaderView = ProfileHeaderView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .lightGray
         title = "Profile"
+        view.addSubview(profileHeaderView)
+    }
+    override func viewWillLayoutSubviews() {
+//        profileHeaderView.frame = view.frame/
+        profileHeaderView.frame = self.view.safeAreaLayoutGuide.layoutFrame
+       
     }
 }
