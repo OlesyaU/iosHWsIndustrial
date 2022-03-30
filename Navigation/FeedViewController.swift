4955df83aeb39f6b8895d16605db272fb9317199
OlesyaU <1600174@mail.ru>
2022-03-30 21:09:47 +0300

    finished homework 2.2 Верстка экранов для iOS. Auto Layout

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -10,28 +10,67 @@ import UIKit
 class FeedViewController: UIViewController {
     
     private let post = Post(title: "Feed")
-    private var goToPostButton: UIButton = {
+    private let stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.backgroundColor = .systemIndigo
+        stack.distribution = .fillEqually
+        stack.axis = .vertical
+        stack.spacing = 10
+        return stack
+        
+    }()
+    
+    private var firstButton: UIButton = {
         let button = UIButton()
-        button.frame = CGRect(x: 100, y: 200, width: 200, height: 50)
         button.layer.cornerRadius = 10
-        button.backgroundColor = .systemRed
-        button.setTitle("Go to Post", for: .normal)
+        button.backgroundColor = .white
+        button.setTitle("First", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(buttonPush(_:)), for: .touchUpInside)
+        return button
+    }()
+    
+    private var secondButton: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 10
+        button.backgroundColor = .black
+        button.setTitle("Second", for: .normal)
         button.setTitleColor(.systemYellow, for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
-//        button.translatesAutoresizingMaskIntoConstraints = false
+        button.translatesAutoresizingMaskIntoConstraints = false
         button.addTarget(self, action: #selector(buttonPush(_:)), for: .touchUpInside)
         return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       view.backgroundColor = .systemMint
+        view.backgroundColor = .systemMint
         title = post.title
         navigationItem.title = post.title
-        view.addSubview(goToPostButton)
+        view.addSubview(stackView)
+        stackViewLayout()
     }
     
     @objc private func buttonPush(_ sender: UIButton) {
         navigationController?.pushViewController(PostViewController(), animated: true)
+    }
+    
+    private func stackViewLayout() {
+        [firstButton, secondButton].forEach{ stackView.addArrangedSubview($0)}
+        
+        NSLayoutConstraint.activate([
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.heightAnchor.constraint(equalToConstant: 100),
+            stackView.widthAnchor.constraint(equalToConstant: 200),
+            
+            firstButton.heightAnchor.constraint(equalToConstant: 50),
+            secondButton.heightAnchor.constraint(equalToConstant: 50),
+            firstButton.widthAnchor.constraint(equalToConstant: 200),
+            secondButton.widthAnchor.constraint(equalToConstant: 200)
+        ])
     }
 }
